a657e4618bde7649c5d8c4031fc182be2ca2a911
Sarah Garlock <garlock.sarah10@gmail.com>
2023-07-29 13:09:35 -0600

    fix spacing

--- spec/facades/event_facade_spec.rb
+++ spec/facades/event_facade_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe EventsFacade, :vcr do
     xit "returns all events" do
       facade = EventsFacade.new
       events = facade.get_all_events
+
       expect(events).to be_an(Array)
       expect(events.first).to be_a(Event)
     end
