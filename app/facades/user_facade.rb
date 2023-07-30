517c03090289d534dc93f581bd5c84834d51bb4b
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-29 22:38:04 -0600

    refactor: update user facade and poro with id

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -2,13 +2,13 @@ class UserFacade
   def all_users
     json = service.user_index
 
-    @users = json[:data].map { |details| User.new(details[:attributes]) }
+    @users = json[:data].map { |details| User.new(details) }
   end
 
   def find_user(user_id)
     json = service.user_show(user_id)
 
-    @user = User.new(json[:data][:attributes])
+    @user = User.new(json[:data])
   end
 
   private
