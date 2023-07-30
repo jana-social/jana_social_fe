3eedbf466eabd9f0e5825d8a7a4261ed3c409d9c
Austin Woods <austinwoods99@gmail.com>
2023-07-30 11:21:47 -0400

    test refactor

--- app/facades/events_facade.rb
+++ app/facades/events_facade.rb
@@ -2,7 +2,7 @@ class EventsFacade
   def get_all_events
     service = RenderService.new
     request = service.get_all_events
-    @events = request[:data].map do |event_data|
+    request[:data].map do |event_data|
       Event.new(event_data)
     end
   end
