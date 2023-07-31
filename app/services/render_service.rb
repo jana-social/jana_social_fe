1fe98f99da0f652a285cffca47e27accdf98f698
Austin Woods <austinwoods99@gmail.com>
2023-07-31 17:51:15 -0400

    Complete create event action

--- app/services/render_service.rb
+++ app/services/render_service.rb
@@ -7,7 +7,7 @@ class RenderService
     get_url("/api/v1/users")
   end
 
-  def user_search(zipcode, radius)
+  def user_search(_zipcode, _radius)
     # need to make the correct call
     get_url("/api/v1/users/search")
   end
@@ -16,14 +16,22 @@ class RenderService
     get_url("/api/v1/users/#{user_id}")
   end
 
+  def create_event(user_id, params)
+    post_url("/api/v1/users/#{user_id}/events", { title: params[:title], description: params[:description], street_address: "#{params[:street_address]}, #{params[:city]}, #{params[:state]}", zipcode: params[:zipcode], date_time: params[:date_time], private: params[:private] })
+  end
+
   def get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def post_url(url, params)
+    conn.post(url, params)
+  end
+
   def conn
-    Faraday.new(url: 'https://jana-social-be.onrender.com/')
-    # Faraday.new(url: "https://localhost:3000")
+    Faraday.new(url: "https://jana-social-be.onrender.com/")
+    # Faraday.new(url: "http://localhost:3000")
     # change to https://localhost:3000 for local testing if needed
   end
 end
