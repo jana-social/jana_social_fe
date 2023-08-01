6c3f48b9fca91fa4299fb940746ac4c00364f738
Jeff Nelson <jpnelson85@gmail.com>
2023-08-01 16:46:08 -0500

    update event show test

--- spec/features/events/show_spec.rb
+++ spec/features/events/show_spec.rb
@@ -9,30 +9,22 @@ RSpec.describe 'Get one event', type: :feature do
   end
 
   it 'displays one event' do
+    json_response = File.read("spec/fixtures/authenticated_user.json")
+    stub_request(:get, "https://jana-social-be.onrender.com/api/v1/search?p=test&q=stefanie@jaskolski-sanford.example")
+      .to_return(status: 200, body: json_response, headers: {})
 
-    # user_1 = User.create!(
-    #   username: "Sweet Bread",
-    #   email: "stefanie@jaskolski-sanford.example",
-    #   password: "test",
-    #   zipcode: "01417",
-    #   street_address: "13575 Lavonda Ways",
-    #   bio: "[\"Five dollar toast sriracha pop-up.\", \"Williamsburg austin mustache kombucha literally ennui pabst.\", \"Umami post-ironic direct trade.\", \"Sustainable diy park seitan pop-up selvage farm-to-table cold-pressed.\", \"Roof yolo blog.\"]",
-    #   likes: "Gentrify roof hoodie mlkshk skateboard.",
-    #   dislikes: "Lo-fi irony you probably haven't heard of them fingerstache tofu.",
-    #   profile_image_link: "http://cummings-murphy.test/kim_hoppe"
-    # )
-    # @event = EventsFacade.new.get_one_event(1)
+    stub_request(:get, "https://jana-social-be.onrender.com/api/v1/users/1")
+      .to_return(status: 200, body: json_response, headers: {})
 
-    # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
+    stub_request(:get, "https://jana-social-be.onrender.com/api/v1/events")
+      .to_return(status: 200, body: json_response, headers: {})
 
-    # visit login_path
+    visit root_path
 
-    # fill_in :username, with: @user1.username
-    # fill_in :password, with: @user1.password
-
-    # click_button "Login"
-    # visit events_path
-
+    fill_in :email, with: "stefanie@jaskolski-sanford.example"
+    fill_in :password, with: "test"
+    click_on "Log In"
+    
     visit '/events/1'
 
     expect(page).to have_content("Casey's Sweet Quiet Gathering")
