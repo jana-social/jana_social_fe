eb99c609210555c240ac58933795035cf059efc7
Sarah Garlock <garlock.sarah10@gmail.com>
2023-08-02 10:27:31 -0600

    comment out future code

--- app/controllers/users/events_controller.rb
+++ app/controllers/users/events_controller.rb
@@ -3,4 +3,20 @@ class Users::EventsController < ApplicationController
     @hosting = EventsFacade.new.get_hosting(params[:format])
     @attending = EventsFacade.new.get_attending(params[:format])
   end
+
+  # def new
+  #   @user = params[:user_id]
+  #   binding.pry
+  # end
+
+  # def create
+  #   user = params[:user_id]
+  #   EventsFacade.new.create_event(user, event_params)
+  # end
+
+  # private
+
+  # def event_params
+  #   params.permit(:title, :description, :street_address, :city, :state, :zipcode, :date_time, :private)
+  # end
 end
