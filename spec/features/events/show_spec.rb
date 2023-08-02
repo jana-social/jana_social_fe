277e2dd6951baf82938da0ffe442771827786fef
Garrett Gregor <garrett.gregor@gmail.com>
2023-08-01 22:12:05 -0600

    format per rubocop

--- spec/features/events/show_spec.rb
+++ spec/features/events/show_spec.rb
@@ -1,14 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Get one event', type: :feature do
-
+RSpec.describe "Get one event", type: :feature do
   before(:each) do
     json_response = File.read("spec/fixtures/get_one_event.json")
     stub_request(:get, "https://jana-social-be.onrender.com/api/v1/events/1")
       .to_return(status: 200, body: json_response)
   end
 
-  it 'displays one event' do
+  it "displays one event" do
     json_response = File.read("spec/fixtures/authenticated_user.json")
     stub_request(:get, "https://jana-social-be.onrender.com/api/v1/search?p=test&q=stefanie@jaskolski-sanford.example")
       .to_return(status: 200, body: json_response, headers: {})
@@ -24,8 +23,8 @@ RSpec.describe 'Get one event', type: :feature do
     fill_in :email, with: "stefanie@jaskolski-sanford.example"
     fill_in :password, with: "test"
     click_on "Log In"
-    
-    visit '/events/1'
+
+    visit "/events/1"
 
     expect(page).to have_content("Casey's Sweet Quiet Gathering")
     expect(page).to have_content("Movie for people with noise sensitivities")
